f12d45f871b0b7860661cf63bda69c91952e35fc
Aswin <invictusasw7@gmail.com>
2025-08-29 00:28:42 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
-    <div>
-      <h1 className="text-3xl">Hello World</h1>
+    <div className="bg-[#202020]">
+       <input type="text" placeholder="enter your name" className="p-3" onChange={(e) => setName(e.target.value)} value={name}/>
+
+       <div className="bg-teal-700 mt-12">
+        {name ? `Hello, ${name}` : 'Please enter your name'}
+       </div>
     </div>
   );
 }
